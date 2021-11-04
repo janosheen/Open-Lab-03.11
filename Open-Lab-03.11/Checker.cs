961f2c1a1dbf03897b997072c668a1753a05d0a1
janosheen <91180541+janosheen@users.noreply.github.com>
2021-11-04 14:08:58 +0100

    I implemented the method that checks if a string is a palindrome.

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 
 namespace Open_Lab_03._11
 {
@@ -6,7 +8,14 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
-        } 
-    }
+            string firstHalf = str.Substring(0, str.Length / 2);
+            char[] arr = str.ToCharArray();
+
+            Array.Reverse(arr);
+
+            string str2 = new string(arr);
+            string secondHalf = str2.Substring(0, str2.Length / 2);
+            return firstHalf.Equals(secondHalf);
+        }
+    } 
 }
